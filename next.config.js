fb31751827ff17097b44011a16046375aebf72b1
elmergustavo <elmergustavo79@gmail.com>
2022-08-21 12:33:43 -0600

    header end

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports={
     images: {
-        domains: ["links.papareact.com", "fakestoreapi.com"]
+        domains: ["links.papareact.com", "fakestoreapi.com", "github.com", "ibb.co","i.ibb.co/F7jG1FW"]
     }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,62 @@
-import React from 'react'
-import Image from "next/image"
+import React from "react";
+import Image from "next/image";
+import {
+  MenuIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/outline";
 
 const Header = () => {
   return (
-    <div className='flex items-center bg-amazon_blue  p-1 flex-grow py-2'>
-      <div className='mt-2 flex items-center flex-grow sm:flex-grow-0'>
-        <Image src="https://links.papareact.com/f90"
-        width={150} 
-        height={40}
-        objectFit="contain"
-        className='cursor-pointer'
-        />
-      </div>
-    </div>
-  )
-}
+    <header>
+      <div className="flex items-center bg-amazon_blue  p-1 flex-grow py-2">
+        <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
+          <Image
+            src="https://links.papareact.com/f90"
+            width={150}
+            height={40}
+            objectFit="contain"
+            className="cursor-pointer"
+          />
+        </div>
 
-export default Header
+        <div className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500">
+          <input
+            type="text"
+            className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4"
+          />
+          <SearchIcon className="h-12 p-4 " />
+        </div>
+
+        <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
+          <div className="link">
+            <p>Hello Gustavo</p>
+            <p className="font-extrabold md:text-sm">Account & List</p>
+          </div>
+          <div className="link">
+            <p>Return</p>
+            <p className="font-extrabold md:text-sm">& Orders</p>
+          </div>
+          <div className="link relative flex items-center">
+            <span className="absolute top-0 right-0 md:right-10 h-5 w-5  bg-yellow-400 text-center rounded-full text-black font-bold 2">
+              99
+            </span>
+            <ShoppingCartIcon className="h-10" />
+            <p className="font-extrabold md:text-sm hidden md:inline mt-2">
+              Basket
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex items-center bg-amazon_blue-light text-white text-sm">
+        <p>
+          <MenuIcon className="h-6 mr-1" />
+          All
+        </p>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
