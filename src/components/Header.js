5483485dbb113868075ea1237fb05190976881ba
elmergustavo <elmergustavo79@gmail.com>
2022-08-22 09:33:39 -0600

    implements banner and card

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ import {
 
 const Header = () => {
   return (
-    <header>
+    <header className="fixed">
       <div className="flex items-center bg-amazon_blue  p-1 flex-grow ">
         <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
           <Image
--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -1,22 +1,66 @@
-import React from 'react'
-import Product from './Product'
+import React from "react";
+import Product from "./Product";
 
-const ProductFeed = ({products}) => {
+const ProductFeed = ({ products }) => {
   return (
-    <div className='grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-24'>
-        {products.map(({id, title, price, description, category, image}) => (
+    <>
+      <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-24 mx-auto">
+        {products
+          .slice(0, 4)
+          .map(({ id, title, price, description, category, image }) => (
             <Product
-            key={id}
-            id={id}
-            title={title}
-            price={price}
-            description={description}
-            category={category}
-            image={image}
+              key={id}
+              id={id}
+              title={title}
+              price={price}
+              description={description}
+              category={category}
+              image={image}
             />
-        ))}
-    </div>
-  )
-}
+          ))}
+      </div>
 
-export default ProductFeed
+      <img
+        className="md:col-span-full mx-auto"
+        src="https://links.papareact.com/dyz"
+        alt=""
+      />
+
+      <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mx-auto">
+        
+        <div className="md:col-span-2">
+        {products
+          .slice(4, 5)
+          .map(({ id, title, price, description, category, image }) => (
+            <Product
+              key={id}
+              id={id}
+              title={title}
+              price={price}
+              description={description}
+              category={category}
+              image={image}
+            />
+          ))}
+        </div>
+       
+
+        {products
+          .slice(4, products.length)
+          .map(({ id, title, price, description, category, image }) => (
+            <Product
+              key={id}
+              id={id}
+              title={title}
+              price={price}
+              description={description}
+              category={category}
+              image={image}
+            />
+          ))}
+      </div>
+    </>
+  );
+};
+
+export default ProductFeed;
